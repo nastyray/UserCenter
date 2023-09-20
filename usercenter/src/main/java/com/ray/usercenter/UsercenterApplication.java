189ff069aedcf89ac736d094bad9ec09fcf511b2
lilray <2313715475@qq.com>
2023-09-20 17:23:05 +0800

    后端初始化

---
+++ usercenter/src/main/java/com/ray/usercenter/UsercenterApplication.java
@@ -0,0 +1,13 @@
+package com.ray.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsercenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsercenterApplication.class, args);
+    }
+
+}
---
+++ usercenter/src/test/java/com/ray/usercenter/UsercenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ray.usercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsercenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
