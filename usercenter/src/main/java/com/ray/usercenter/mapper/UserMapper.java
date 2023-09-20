ab799be7df03e944465c7b0547c7e2916027c853
lilray <2313715475@qq.com>
2023-09-20 18:06:49 +0800

    Mybatis-plus测试

---
+++ usercenter/src/main/java/com/ray/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.ray.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ray.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
