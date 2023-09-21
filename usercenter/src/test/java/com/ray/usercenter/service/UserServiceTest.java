c4a0efe63f4188e15051eab3bb663f9e0e4212ad
lilray <2313715475@qq.com>
2023-09-21 12:27:08 +0800

    Mybatis-plus测试

--- usercenter/src/test/java/com/ray/usercenter/service/UserServiceTest.java
+++ usercenter/src/test/java/com/ray/usercenter/service/UserServiceTest.java
@@ -1,7 +1,12 @@
 package com.ray.usercenter.service;
+import java.util.Date;
 
+import com.ray.usercenter.model.domain.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,8 +26,22 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 @SpringBootTest
 public class UserServiceTest {
+    @Resource
+    private UserService userService;
     @Test
     public void testAddUser(){
+        User user = new User();
+        user.setUsername("dogray");
+        user.setUserAccount("123");
+        user.setAvatarUrl("https://github.com/nastyray/UserCenter/blob/main/Image/1693641416492.jpg");
+        user.setGender(0);
+        user.setUserPassword("xxx");
+        user.setEmail("456");
+        user.setUserStatus(0);
+        user.setPhone("123");
+        boolean result = userService.save(user);
+        System.out.println(user.getId());
+        assertTrue(result);
 
     }
 
