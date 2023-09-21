77fa731492a6903f2004f7fa3f2c1ed643df55b8
lilray <2313715475@qq.com>
2023-09-21 14:01:04 +0800

    用户注册逻辑

--- usercenter/src/main/java/com/ray/usercenter/service/UserService.java
+++ usercenter/src/main/java/com/ray/usercenter/service/UserService.java
@@ -10,4 +10,13 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface UserService extends IService<User> {
 
+    /**
+     * 用户注册
+     * @param userAccount 用户账户
+     * @param userPassword 用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户Id
+     */
+    long userRegister(String userAccount,String userPassword,String checkPassword);
+
 }
--- usercenter/src/test/java/com/ray/usercenter/service/UserServiceTest.java
+++ usercenter/src/test/java/com/ray/usercenter/service/UserServiceTest.java
@@ -45,4 +45,34 @@ public class UserServiceTest {
 
     }
 
+    @Test
+    void userRegister() {
+        String userAccount = "yupi";
+        String userPassword = "";
+        String checkPassword = "123456";
+        long result = userService.userRegister(userAccount, userPassword, checkPassword);
+        Assertions.assertEquals(-1, result);
+        userAccount = "yu";
+        result = userService.userRegister(userAccount, userPassword, checkPassword);
+        Assertions.assertEquals(-1, result);
+        userAccount = "yupi";
+        userPassword = "123456";
+        result = userService.userRegister(userAccount, userPassword, checkPassword);
+        Assertions.assertEquals(-1, result);
+        userAccount = "yu pi";
+        userPassword = "12345678";
+        result = userService.userRegister(userAccount, userPassword, checkPassword);
+        Assertions.assertEquals(-1, result);
+        checkPassword = "123456789";
+        result = userService.userRegister(userAccount, userPassword, checkPassword);
+        Assertions.assertEquals(-1, result);
+        userAccount = "dogYupi";
+        checkPassword = "12345678";
+        result = userService.userRegister(userAccount, userPassword, checkPassword);
+        Assertions.assertEquals(-1, result);
+        userAccount = "yupi";
+        result = userService.userRegister(userAccount, userPassword, checkPassword);
+        Assertions.assertEquals(-1, result);
+
+    }
 }
