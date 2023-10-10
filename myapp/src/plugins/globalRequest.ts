a847a7686b3fe12d324666c0125c0191e6cbb762
lilray <2313715475@qq.com>
2023-10-10 15:48:10 +0800

    线上前后端联调
    线上使用测试，修改bug

--- myapp/src/plugins/globalRequest.ts
+++ myapp/src/plugins/globalRequest.ts
@@ -12,7 +12,7 @@ import {stringify} from "querystring";
  */
 const request = extend({
     credentials: 'include', // 默认请求是否带上cookie
-    prefix: process.env.NODE_ENV === 'production' ? 'http://175.178.249.250:8080' : undefined
+    prefix: process.env.NODE_ENV === 'production' ? 'http://175.178.249.250' : undefined
     // requestType: 'form',
 });
 
